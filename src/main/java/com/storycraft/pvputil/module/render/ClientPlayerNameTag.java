535dfe118905a0be1de95043fa5ac81d5a82d32c
storycraft <dbsxoal123@naver.com>
2018-11-25 16:51:42 +0900

    idfk why but fix

--- src/main/java/com/storycraft/pvputil/module/render/ClientPlayerNameTag.java
+++ src/main/java/com/storycraft/pvputil/module/render/ClientPlayerNameTag.java
@@ -6,6 +6,7 @@ import com.storycraft.pvputil.module.IModule;
 import com.storycraft.pvputil.util.reflect.Reflect;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
@@ -25,21 +26,10 @@ public class ClientPlayerNameTag implements IModule {
 
     public static final String OPTION_CATEGORY = "render";
 
-    private static Reflect.WrappedMethod<Void, RenderPlayer> renderOffsetLivingLabel;
-
-    private static Reflect.WrappedField<Timer, Minecraft> timer;
-
-    private static Reflect.WrappedField<Double, RenderManager> renderPosX;
-    private static Reflect.WrappedField<Double, RenderManager> renderPosY;
-    private static Reflect.WrappedField<Double, RenderManager> renderPosZ;
+    private static Reflect.WrappedMethod<Void, RenderPlayer> renderEntityName;
 
     static {
-        renderOffsetLivingLabel = Reflect.getMethod(RenderPlayer.class, "renderOffsetLivingLabel", "func_177069_a", AbstractClientPlayer.class, double.class, double.class, double.class, String.class, float.class, double.class);
-        timer = Reflect.getField(Minecraft.class, "timer", "field_71428_T");
-
-        renderPosX = Reflect.getField(RenderManager.class, "renderPosX", "field_78725_b");
-        renderPosY = Reflect.getField(RenderManager.class, "renderPosY", "field_78726_c");
-        renderPosZ = Reflect.getField(RenderManager.class, "renderPosZ", "field_78723_d");
+        renderEntityName = Reflect.getMethod(RenderPlayer.class, "renderEntityName", "func_188296_a", AbstractClientPlayer.class, double.class, double.class, double.class, String.class, double.class);
     }
 
     private PvpUtil mod;
@@ -68,52 +58,17 @@ public class ClientPlayerNameTag implements IModule {
 
     @SubscribeEvent
     public void onPostRender(RenderPlayerEvent.Post e){
-        if (modEnabled && minecraft.gameSettings.thirdPersonView != 0 && e.getEntityPlayer().isUser()) {
+        if (e.getEntityPlayer().isUser() && modEnabled && minecraft.gameSettings.thirdPersonView != 0) {
             EntityPlayer entity = e.getEntityPlayer();
 
-            double d0 = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * e.getPartialRenderTick();
-            double d1 = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * e.getPartialRenderTick();
-            double d2 = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * e.getPartialRenderTick();
+            double d0 = entity.getDistanceSq(e.getRenderer().getRenderManager().renderViewEntity);
+            float f = entity.isSneaking() ? RenderPlayer.NAME_TAG_RANGE_SNEAK : RenderPlayer.NAME_TAG_RANGE;
 
-            double x = d0 - renderPosX.get(e.getRenderer().getRenderManager());
-            double y = d1 - renderPosY.get(e.getRenderer().getRenderManager());
-            double z = d2 - renderPosZ.get(e.getRenderer().getRenderManager());
-
-            String s = entity.getDisplayName().getFormattedText();
-            GlStateManager.alphaFunc(516, 0.1F);
-
-            if (entity.isSneaking()) {
-                FontRenderer fontrenderer = e.getRenderer().getFontRendererFromRenderManager();
-                GlStateManager.pushMatrix();
-                GlStateManager.translate(x, y + entity.height + 0.5F - (entity.isChild() ? entity.height / 2.0F : 0.0F), z);
-                GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-                GlStateManager.rotate(((float) Math.pow(-1, minecraft.gameSettings.thirdPersonView)) * e.getRenderer().getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-                GlStateManager.rotate(e.getRenderer().getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
-                GlStateManager.scale(-0.02666667F, -0.02666667F, 0.02666667F);
-                GlStateManager.translate(0.0F, 9.374999F, 0.0F);
-                GlStateManager.disableLighting();
-                GlStateManager.depthMask(false);
-                GlStateManager.enableBlend();
-                GlStateManager.disableTexture2D();
-                GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-                int i = fontrenderer.getStringWidth(s) / 2;
-                Tessellator tessellator = Tessellator.getInstance();
-                BufferBuilder worldrenderer = tessellator.getBuffer();
-                worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
-                worldrenderer.pos((double) (-i - 1), -1.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-                worldrenderer.pos((double) (-i - 1), 8.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-                worldrenderer.pos((double) (i + 1), 8.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-                worldrenderer.pos((double) (i + 1), -1.0D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-                tessellator.draw();
-                GlStateManager.enableTexture2D();
-                GlStateManager.depthMask(true);
-                fontrenderer.drawString(s, -fontrenderer.getStringWidth(s) / 2, 0, 553648127);
-                GlStateManager.enableLighting();
-                GlStateManager.disableBlend();
-                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-                GlStateManager.popMatrix();
-            } else {
-                renderOffsetLivingLabel.invoke(e.getRenderer(), entity, x, y - (entity.isChild() ? (double) (entity.height / 2.0F) : 0.0D), z, s, 0.02666667F, 0);
+            if (d0 < (double)(f * f))
+            {
+                String s = entity.getDisplayName().getFormattedText();
+                GlStateManager.alphaFunc(516, 0.1F);
+                renderEntityName.invoke(e.getRenderer(), ((EntityPlayerSP) entity), e.getX(), e.getY(), e.getZ(), s, d0);
             }
         }
     }
